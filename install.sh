7ff7a5c9d3f7b4ddd7f36c46d478ff4056235eb9
sunliang <sunliang711@163.com>
2021-01-28 22:20:52 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+link="https://gitee.com/sunliang711/fetcher/attach_files/602033/download/fetcher-Linux-x86_64.tar.bz2"
+
+install(){
+    dest=${1:?'missing install location'}
+    if [ ! -d ${dest} ];then
+        echo "Create ${dest}..."
+        mkdir -p ${dest}
+    fi
+    dest="$(realpath $dest)"
+
+    tarFile="${link##*/}"
+
+    cd /tmp
+    if [ ! -e ${tarFile} ];then
+        echo "Download fetcher: ${link} ..."
+        curl -LO "${link}" || { echo "Download failed"; exit 1; }
+    fi
+
+    tar -C ${dest} -jxvf ${tarFile}
+
+}
+
+install $1
